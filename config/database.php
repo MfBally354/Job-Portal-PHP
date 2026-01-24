f156aa543344d41ac7f0858ae2f84ba684b0b909
MfBally354 <iqbalguntur313@gmail.com>
2026-01-24 20:21:31 +0700

    Auto commit

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 // config/database.php
 // ==========================================
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_USER', 'iqbal');
 define('DB_PASS', '');
 define('DB_NAME', 'job_portal_v2');
 
